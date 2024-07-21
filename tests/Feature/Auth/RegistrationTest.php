c992c00c828e155e39f137265eb9e8e4be93434c
ujikstark <ujikbauk@gmail.com>
2024-07-21 15:19:20 +0700

    update registration test

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Auth;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+
 
 class RegistrationTest extends TestCase
 {
@@ -27,5 +29,23 @@ class RegistrationTest extends TestCase
 
         $this->assertAuthenticated();
         $response->assertRedirect(route('dashboard', absolute: false));
+    }
+
+    public function test_new_user_cannot_register_with_used_email(): void
+    {
+        User::factory()->create(['email' => 'test@example.com']);
+
+        $this->get('/register');
+
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $this->assertGuest();
+        $response->assertRedirect(route('register'));
+        $response->assertSessionHasErrors('email');
     }
 }
